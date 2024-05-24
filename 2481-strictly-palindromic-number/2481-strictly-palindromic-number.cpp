1ced3452e88009dd2f8c683575e740d355db47da
KrishnaTulasiSatti <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2024-05-24 10:47:06 +0530

    Time: 3 ms (21.19%), Space: 7 MB (62.23%) - LeetHub

---
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        return false;
+        // In base n-2 : for every number , we will get the bin representation as 12 which is not palindromic
+    }
+};
