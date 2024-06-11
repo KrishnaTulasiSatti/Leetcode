f7f921f9b21fff6912d693926902962e62db3999
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2024-06-11 19:12:05 +0530

    Time: 2 ms (61.59%), Space: 10.3 MB (24.9%) - LeetHub

---
+++ 1217-relative-sort-array/1217-relative-sort-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+       map<int,int>m;
+        vector<int>ans;
+        for(int i = 0 ; i < arr1.size() ; i++) m[arr1[i]]++;
+        for(int i = 0 ; i < arr2.size() ; i++) {
+            while(m[arr2[i]]) {
+                ans.push_back(arr2[i]);
+                m[arr2[i]]--;
+            }
+        }
+        for(auto it : m) {
+            while(it.second) {
+                ans.push_back(it.first);
+                it.second--;
+            }
+        }
+        return ans;
+    }
+};
