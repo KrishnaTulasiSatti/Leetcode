d5bf4c8b4f408d9e43114611551e0ecce207764a
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2024-07-16 10:22:54 +0530

    Time: 0 ms (100%), Space: 8.1 MB (54.99%) - LeetHub

---
+++ 3484-lexicographically-smallest-string-after-a-swap/3484-lexicographically-smallest-string-after-a-swap.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string getSmallestString(string s) {
+        for(int i = 0 ; i < s.size()-1 ;  i++) {
+           if(((s[i]%2==0) && (s[i+1]%2==0)) || ((s[i]%2)) && (s[i+1]%2)) {
+            if(s[i] > s[i+1]) {
+                swap(s[i],s[i+1]);
+                return s;
+            }
+           }
+        }
+        return s;
+    }
+};
