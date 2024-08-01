21dfeb78db42b8083debaad2155a56d0232ca724
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2024-08-01 19:45:20 +0530

    Time: 13 ms (24.23%), Space: 18.2 MB (5.49%) - LeetHub

--- 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
-        vector<vector<int>>res;
-        multimap<int,int>m;
-        for(int i = 0 ; i < groupSizes.size() ; i++) {
-           m.insert(pair<int,int>(groupSizes[i],i));
-        }
-        for(auto it = m.begin() ; it != m.end() ; ) {
-            int k = it->first;
-            vector<int>ans;
-            while(k) {
-                ans.push_back(it->second);
-                it++;
-                k--;
+       multimap<int,int>m;
+       for(int i = 0 ; i < groupSizes.size() ; i++) {
+          m.insert(pair<int,int>(groupSizes[i],i));
+       }
+       vector<vector<int>>ans;
+       int prev = 0;
+       vector<int>v;
+      // for(auto it : m) cout<<it.first<<" "<<it.second;
+       for(auto it : m) {
+         if(it.first != prev) {
+            v = {};
+            v.push_back(it.second);
+            prev = it.first;
+         }
+         else {
+            v.push_back(it.second);
+         }
+         if(v.size() == it.first) {
+                ans.push_back(v);
+                v = {};
             }
-            res.push_back(ans);
-        }
-        return res;
+       }
+       return ans;
     }
 };
