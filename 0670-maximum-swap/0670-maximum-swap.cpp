c68d025ead61979eb4d388992862240b7b75267b
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2024-08-21 12:29:43 +0530

    Time: 3 ms (37.99%), Space: 7.5 MB (46.65%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+       string str = to_string(num);
+       string temp = str;
+       string ans = str;
+       for(int i = 0 ; i < str.size() ; i++) {
+        for(int j = i+1 ; j < str.size() ; j++) {
+           swap(str[i],str[j]);
+           if(str > ans) ans = str;
+           str = temp;
+        }
+       }
+       return stol(ans);
+    }
+};
