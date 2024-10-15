b375a4a2acd245f478f267133abe97b3c36dad1a
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2024-10-15 20:20:28 +0530

    Time: 181 ms (10.94%), Space: 41.8 MB (12.23%) - LeetHub

--- 3453-generate-binary-strings-without-adjacent-zeros/3453-generate-binary-strings-without-adjacent-zeros.cpp
+++ 3453-generate-binary-strings-without-adjacent-zeros/3453-generate-binary-strings-without-adjacent-zeros.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-bool isValid(string s) {
-    for(int i = 1 ; i < s.size() ; i++) {
-        if(s[i-1] == '0' && s[i] == '0') return false;
-    }
-    return true;
-}
     vector<string> validStrings(int n) {
-        vector<string>res;
-        for(int i = 0 ; i < pow(2,n) ; i++) {
-            string bin = "";
+        vector<string>bin;
+        for(int i = 0 ; i < (1 << n) ; i++) {
+            string res = "";
+            int flag = 1;
             for(int j = 0 ; j < n ; j++) {
-                 int mask = i & (1 << j);  // Kth bit set --> n & (1 << k)
-                 if(mask) bin+='1';
-                 else bin+='0';
+                int mask = 1 << j;
+                if(i & (1 << j)) {
+                  res+='1';
+                }
+                else res+='0';
+
+                if(j >= 2) {
+                   if(res[j-1] == '0' && res[j-2] == '0') flag = 0; 
+                }
             }
-            if(isValid(bin)) res.push_back(bin);
+            int a = res.size();
+            if(a >= 2 && res[a-1] == '0' && res[a-2] == '0') flag = 0;
+            if(flag) bin.push_back(res);
         }
-        return res;
+        return bin;
     }
 };
