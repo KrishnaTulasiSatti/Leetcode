518bf14b6224814d8e95837d9a3e6803b4dce803
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2024-10-21 10:03:43 +0530

    Time: 0 ms (100%), Space: 58 MB (50.03%) - LeetHub

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int numOfSubarrays(vector<int>& arr, int k, int threshold) {
-       int l = 0;
-       int r = k-1;
-       int sum = 0;
-       int cnt = 0;
-       for(int i = 0 ; i <= r ; i++) sum+=arr[i];
-       while(r < arr.size()-1) {
-          int avg = sum/k;
-          if(avg >= threshold) cnt++;
-          sum-=arr[l];
-          l++;
-          r++;
-          sum+=arr[r];
-       }
-       int avg = sum/k;
-       if(avg >= threshold) cnt++;
-       return cnt;
+        int sum = 0;
+        int cnt = 0;
+        int l = 0;
+        int r = k;
+        for(int i = 0 ; i < k ; i++) sum+=arr[i];
+        int avg = sum/k;
+        if(avg >= threshold) cnt++;
+        while(r < arr.size()) {
+            sum+=arr[r];
+            sum-=arr[l];
+            avg = sum/k;
+            if(avg >= threshold) cnt++;
+            l++;
+            r++;
+
+        }
+        return cnt;
+
     }
 };
