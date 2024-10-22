15ed8fe5d8f739557418a32b3586ceaa4cc80b10
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2024-10-22 15:45:27 +0530

    Time: 23 ms (60.44%), Space: 11.3 MB (64.33%) - LeetHub

--- 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.cpp
+++ 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
 int fun(int n) {
     if(n == 1) return 0;
-    if(n % 2) return 1+fun(3*n+1);
-    else return 1+fun(n/2);
+    if(n % 2) return 1+fun(n*3+1);
+    return 1+fun(n/2);
+     
 }
     int getKth(int lo, int hi, int k) {
-        vector<pair<int,int>>power;
+        vector<pair<int,int>>v;
         for(int i = lo ; i <= hi ; i++) {
-            int p = fun(i);
-            power.push_back({p,i});
+            v.push_back({fun(i),i});
         }
-      // for(auto it : power) cout<<it.first<<" "<<it.second<<endl;
-        sort(power.begin(),power.end());
-        return power[k-1].second;
-
+        sort(v.begin(),v.end());
+        return v[k-1].second;
     }
 };
