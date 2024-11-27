61119d68c5599ec1bac35180d7e4d2a9b5ff2967
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2024-11-27 10:13:16 +0530

    Time: 176 ms (52.38%), Space: 170.3 MB (15.45%) - LeetHub

--- 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.cpp
+++ 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.cpp
@@ -1,40 +1,51 @@
 class Solution {
 public:
-int fun(vector<int>adj[],int n) {
-        vector<int>dis(n+1,INT_MAX);
-        dis[0] = 0;
-        vector<int>vis(n+1,0);
-        vis[0] = 1;
-        queue<pair<int,int>>q;
-        q.push({0,0});
-        while(!q.empty()) {
-            auto top = q.front();
-            q.pop();
-            int node = top.first;
-            int d = top.second;
-            for(auto it : adj[node]) {
-                if(vis[it] == 0) {
-                    vis[it] = 1;
-                    dis[it] = d+1;
-                    q.push({it,d+1});
-                }
+
+int dijkstra(int src,vector<vector<int>>&adjList,int n) {
+
+    vector<int>vis(n,-1);
+    vector<int>dist(n,INT_MAX);
+
+    dist[src] = 0;
+    vis[src] = 1;
+
+    queue<int>q;
+    q.push(src);
+
+    while(!q.empty()) {
+        int top = q.front();
+        q.pop();
+        for(auto it : adjList[top]) {
+            if(dist[top]+1 < dist[it] && vis[it] == -1) {
+                dist[it] = dist[top]+1;
+                vis[it] = 1;
+                q.push(it);
             }
         }
-        return dis[n-1];
+    }
+
+    return dist[n-1];
+
 }
     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
-        vector<int>adj[n+1];
-        for(int i = 0 ; i < n-1 ; i++) {
-            adj[i].push_back(i+1);
-        }
-        vector<int>res;
-        for(int i = 0 ; i < queries.size() ; i++){
-            int u = queries[i][0];
-            int v = queries[i][1];
-            adj[u].push_back(v);
-            int ans = fun(adj,n);
-            res.push_back(ans);
-        }
-        return res;
+       vector<vector<int>>adjList(n+1);
+       for(int i = 0 ; i < n-1 ; i++) {
+          int u = i;
+          int v = i+1;
+          adjList[u].push_back(v);
+
+       }
+       vector<int>res;
+       for(int i = 0 ; i < queries.size() ; i++) {
+           int u = queries[i][0];
+           int v = queries[i][1];
+
+           adjList[u].push_back(v);
+
+           int ans = dijkstra(0,adjList,n);
+           res.push_back(ans);
+       }
+       return res;
+
     }
 };
