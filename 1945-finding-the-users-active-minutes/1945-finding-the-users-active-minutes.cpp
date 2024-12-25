68eb5576d9a1350cbb978bec7d4f6e9447cf5386
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2024-12-25 15:34:50 +0530

    Time: 64 ms (48.04%), Space: 95 MB (36.27%) - LeetHub

--- 1945-finding-the-users-active-minutes/1945-finding-the-users-active-minutes.cpp
+++ 1945-finding-the-users-active-minutes/1945-finding-the-users-active-minutes.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     vector<int> findingUsersActiveMinutes(vector<vector<int>>& logs, int k) {
-        unordered_map<int,set<int>>m;
-        for(auto it : logs) {
-            m[it[0]].insert(it[1]);
-        }
-        vector<int>res(k,0);
-        unordered_map<int,int>m2;
-        for(auto it : m) {
-            m2[it.second.size()]++;
-        }
-        for(auto it : m2) {
-            //cout<<it.first<<" "<<it.second<<endl;
-            res[it.first-1] = it.second;
-        }
-        return res;
+       map<int,set<int>>m;
+       for(int i = 0 ; i < logs.size() ; i++) {
+          int u = logs[i][0];
+          int t = logs[i][1];
+          m[u].insert(t);
+       }
+
+
+       vector<int>res(k,0);
+       for(auto it : m) {
+          int s = it.second.size();
+          res[s-1]++;
+       } 
+       return res;
+
     }
 };
