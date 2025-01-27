ae705df2ee9da090f84627bf99740f7a11ec8a95
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2025-01-27 19:01:13 +0530

    Time: 7 ms (26.79%), Space: 98.9 MB (97.99%) - LeetHub

--- 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.cpp
+++ 2394-count-subarrays-with-score-less-than-k/2394-count-subarrays-with-score-less-than-k.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, long long k) {
-        long long score = 0;
-        long long sum = 0;
-        long long l = 0;
-        long long r = 0;
         long long cnt = 0;
+        long long sum = 0;
+
+        int l = 0;
+        int r = 0;
+        
+        long long pro = 1;
         while(r < nums.size()) {
             sum+=nums[r];
-          
-            while(sum >= k*1.0/(r-l+1) && l < nums.size()) {
+            pro = sum*(r-l+1);
+
+            while(pro >= k && l < nums.size()) {
                 sum-=nums[l];
                 l++;
-
+                pro = sum*(r-l+1);
                 
-            } 
-           // cout<<r<<" "<<l<<endl;
-            cnt+=(r-l+1);
+            }   
+            if(sum < k ) cnt+=(r-l+1);
             r++;
         }
         return cnt;
+
+        
     }
 };
