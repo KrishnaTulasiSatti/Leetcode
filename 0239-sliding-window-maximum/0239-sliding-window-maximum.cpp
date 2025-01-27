c457a0920ade74b4d92bf045b08a50b616239ed0
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2025-01-27 17:37:43 +0530

    Time: 407 ms (5.7%), Space: 216.3 MB (5.91%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,47 +1,22 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        // vector<int>res;
-        // set<pair<int,int>>s;
-
-        // for(int i = 0 ; i < k ; i++) {
-        //     s.insert({nums[i],i});
-        // }
-
-        // res.push_back(s.rbegin()->first);
-
-        // for(int i = k ; i < nums.size() ; i++) {
-        //     s.erase({nums[i-k],i-k});
-        //     s.insert({nums[i],i});
-        //     res.push_back(s.rbegin()->first);
-        // }
-        // // Since set stores in sorted order , we can simply access the last element
-        // //   s.rbegin() and the first element by s.begin(), but we have to use the 
-        // //   set<pair<int,int>> for handling duplicates 
-        // return res;
-
-
-        // O(n * log k)
-
-
-        // Can be reduced by using deque
-
         vector<int>res;
-        deque<int>dq;
-
+        int l = 0;
+        int r = k;
+        multiset<int>s;
         for(int i = 0 ; i < k ; i++) {
-            while(!dq.empty() && nums[dq.back()] < nums[i]) dq.pop_back();
-            dq.push_back(i);
-        }
-        res.push_back(nums[dq.front()]);
-        
-        for(int i = k ; i < nums.size() ; i++) {
-            if(i-k == dq.front()) dq.pop_front();
-            while(!dq.empty() && nums[dq.back()] < nums[i]) dq.pop_back();
-            dq.push_back(i);
-            res.push_back(nums[dq.front()]);
+            s.insert(nums[i]);
+        } 
+        res.push_back(*s.rbegin());
+        while(r < nums.size()) {
+            s.insert(nums[r]);
+            s.erase(s.find(nums[l]));
+            res.push_back(*s.rbegin());
+            l++;
+            r++;
         }
         return res;
-        
+
     }
 };
