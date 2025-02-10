947e6a96cdb6bb4d6e9606c0da47b742f82300dc
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2025-02-10 22:06:23 +0530

    Time: 3 ms (15.28%), Space: 9.1 MB (41.76%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.cpp
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -3,16 +3,20 @@ public:
     string clearDigits(string s) {
         stack<char>st;
         for(int i = 0 ; i < s.size() ; i++) {
-            if(s[i] >= '0' && s[i] <= '9') {
+            if(s[i] >= '0' and s[i] <= '9') {
                 st.pop();
             }
-            else st.push(s[i]);
+            else {
+                st.push(s[i]);
+            }
         }
-        string res;
+
+        string res = "";
         while(!st.empty()) {
             res+=st.top();
             st.pop();
         }
+
         reverse(res.begin(),res.end());
         return res;
     }
