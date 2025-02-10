5fdc03e23c376b7f1062f1e6953d008692e5f42f
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2025-02-10 22:18:00 +0530

    Time: 0 ms (100%), Space: 11.8 MB (54.96%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        
-        // State : No of ways to reach (i,j) from (0,0) without obstacles
-       // Transition : 
-    //          dp[i][j] = dp[i-1][j]+dp[i][j-1] if (i-1,j) = 0 (i,j-1) = 0
-    //          dp[i][j] = d[i-1][j]  if(i-1,j) = 0
-    //          dp[i][j] = dp[i][j-1] if(i,j-1) = 0
-       // Base case : (0,0) = 1 return 0
-       //             (0,1) = 1 && (1,0) = 1 return 0;
-       // Final Ans : dp[m-1][n-1]
-       int dp[1000][1000];
-       memset(dp,0,sizeof(dp));
-       int m = grid.size();
-       int n = grid[0].size();
-       if(grid[0][0] == 1) return 0;
-       else dp[0][0] = 1;
-       if(m > 1 && n > 1) {
-         if(grid[0][1] == 1 && grid[1][0] == 1) return 0;
-       }
-       for(int i = 0 ; i < m ; i++) {
-         for(int j = 0 ; j < n ; j++) {
-            if(i == 0 && j == 0) continue;
-            int a = 0,b = 0;
-            if(grid[i][j] == 1) dp[i][j] = 0;
-            else{
-                if((i-1) >= 0) a = dp[i-1][j];
-                if((j-1) >= 0) b = dp[i][j-1];
-                dp[i][j] = a+b;
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,0));
+
+        dp[0][0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+
+        for(int i = 1 ; i < m ; i++) {
+            if(obstacleGrid[0][i] == 1) {
+                dp[0][i] = 0;
             }
-         }
-       }
-       return dp[m-1][n-1];
+            else dp[0][i] = dp[0][i-1];
+
+        }
+
+        for(int i = 1 ; i < n ; i++) {
+             if(obstacleGrid[i][0] == 1) {
+                dp[i][0] = 0;
+            }
+            else dp[i][0] = dp[i-1][0];
+        }
+        
+        for(int i = 1 ; i < n ; i++) {
+            for(int j = 1 ; j < m ; j++) {
+                if(obstacleGrid[i][j] == 1) continue;
+                dp[i][j] = dp[i-1][j]+dp[i][j-1];
+            }
+        }
+
+        return dp[n-1][m-1];
+       
     }
 };
