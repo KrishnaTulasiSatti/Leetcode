d5f21b9042a947037521f08d40dc422f697d1bed
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2025-03-01 09:30:06 +0530

    Time: 1206 ms (29.28%), Space: 0B (100%) - LeetHub

---
+++ 1523-capital-gainloss/1523-capital-gainloss.sql
@@ -0,0 +1,7 @@
+/* Write your PL/SQL query statement below */
+select stock_name,sum(
+case when operation = 'Buy' then -1*price
+     else price
+     end) as capital_gain_loss
+from stocks
+group by stock_name;
