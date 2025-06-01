78cb547f17ea9f4bc300dcb12886a05ccfc7b985
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2025-06-01 10:30:10 +0530

    Time: 11 ms (35.93%), Space: 23.6 MB (46.47%) - LeetHub

---
+++ 3843-partition-array-into-two-equal-product-subsets/3843-partition-array-into-two-equal-product-subsets.cpp
@@ -0,0 +1,35 @@
+typedef __int128 ll;
+class Solution {
+public:
+    bool find(vector<int>&nums,ll target) {
+
+        int n = nums.size();
+
+        for(int i = 0 ; i < (1 << n) ; i++) {
+            ll sum = 1;
+            for(int j = 0 ; j < n ; j++) {
+            if(i & (1 << j)) {
+                sum *= nums[j];
+                if(sum == target) return true;
+            }
+        }
+    }
+    
+    return false;
+    }
+    bool checkEqualPartitions(vector<int>& nums, long long target) {
+        ll totalSum = 1;
+
+        for(int i = 0 ; i < nums.size() ; i++) {
+            
+            totalSum = (totalSum*nums[i]);
+            if (totalSum > (ll)target*target) return false; 
+        }
+
+        bool f1 = find(nums,target);
+        if(f1 == false) return false;
+        ll rem = totalSum/target;
+        if(rem == target) return true;
+        return false;
+    }
+};
