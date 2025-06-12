afdc3032a77091fa894c26e60dfdbf6eaa4e57dc
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2025-06-12 18:37:35 +0530

    Time: 3 ms (71.8%), Space: 16 MB (28.34%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
+
 vector<string>res;
 
-void fun(int open,string str,int close) {
-    if(open == 0 && close == 0) {
-        res.push_back(str);
+void fun(string s,int open,int close,int n) {
+    if(open == n && close == n) {
+        res.push_back(s);
         return;
     }
 
-    if(open > 0) fun(open-1,str+'(',close);
-    if(close > open) fun(open,str+')',close-1);
+    // If open < n then we can add '('
+
+    if(open < n) {
+        fun(s+'(',open+1,close,n);
+    }
+
+    // close < open then only we can add ')'
+
+    if(close < open && close < n) {
+        fun(s+')',open,close+1,n);
+    } 
 }
     vector<string> generateParenthesis(int n) {
-        string str = "(";
-        fun(n-1,str,n);
-
+        string s="";
+        fun(s,0,0,n);
         return res;
-
     }
 };
