03282a01df95b811a12e4ce0a254d7bfac6b2d8e
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2025-06-14 18:42:48 +0530

    Time: 7 ms (8.73%), Space: 16.3 MB (5.42%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
+
+int fun(vector<vector<int>>&mat,int i,int j,int m,int n,vector<vector<int>>&dp) {
+    if(i == m-1 && j < n && j >= 0) return mat[i][j];
+
+    if(i < 0 || j < 0 || i >= m || j >= n) return 1e9;
+
+    if(dp[i][j] != INT_MIN) return dp[i][j];
+
+    int dl = mat[i][j] + fun(mat,i+1,j-1,m,n,dp);
+    int b = mat[i][j] + fun(mat,i+1,j,m,n,dp);
+    int dr = mat[i][j] + fun(mat,i+1,j+1,m,n,dp);
+
+    return dp[i][j] = min({dl,b,dr}); 
+}
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        
-         int n = matrix.size();
-         int m = matrix[0].size();
+       
+       int mini = INT_MAX;
 
-         int mini = INT_MAX;
-        for(int i = 1 ; i < matrix.size() ; i++) {
-            for(int j = 0 ; j < matrix[i].size() ; j++) {
-                int left = INT_MAX,up = INT_MAX,right = INT_MAX;
-                
-                if(i-1 < n && i-1 >= 0 && j-1 < m && j-1 >= 0) {
-                    left = matrix[i-1][j-1];
-                    cout<<left<<" ";
-                }
-                if(i-1 < n && i-1 >= 0 && j < m && j >= 0) {
-                    up = matrix[i-1][j];
-                    cout<<up<<" ";
-                }
-                if(i-1 < n && i-1 >= 0 && j+1 < m && j+1 >= 0) {
-                    right = matrix[i-1][j+1];
-                    cout<<right<<" ";
-                }
-              //  cout << min({left,up,right}) <<" ";
-                matrix[i][j] += min({left,up,right});
-            }
-        }
+       int m = matrix.size();
+       int n = matrix[0].size();
 
-        for(int i = 0 ; i < matrix[0].size() ; i++) {
-            mini = min(mini,matrix[n-1][i]);
-        }
-        return mini;
-        
+       vector<vector<int>>dp(m,vector<int>(n,INT_MIN));
+
+       for(int i = 0 ; i < n ; i++){
+           mini = min(mini,fun(matrix,0,i,m,n,dp));
+       }
+
+       return mini;
     }
 };
