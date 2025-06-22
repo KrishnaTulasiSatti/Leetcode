afeb14eb88063e19c23ce998e5b72205e0e2c03a
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2025-06-22 10:26:09 +0530

    Time: 0 ms (100%), Space: 10.4 MB (9.1%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        for(int i = 0 ; i < matrix.size() ; i++) {
-            for(int j = i ; j < matrix.size() ; j++) {
+
+        // Brute : ans[i][j] = matrix[j][n-1-i];
+
+        // Optimal : Tranpose and Reverse
+
+        /*
+           1 2 3  1 4 7
+           4 5 6  2 5 8
+           7 8 9  3 6 9 
+           
+           Diagonal Elements remains same
+
+        */
+
+        int n = matrix.size();
+
+        for(int i = 1 ; i < n ; i++) {
+            for(int j = 0 ; j < i ; j++) {
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
 
-        for(int i = 0 ; i < matrix.size() ; i++) {
+        for(int i = 0 ; i < n ; i++) {
             reverse(matrix[i].begin(),matrix[i].end());
         }
+
     }
 };
