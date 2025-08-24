6f42095c6b934426c163dfc7881c0feeaa8e86dd
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2025-08-24 10:36:19 +0530

    Time: 0 ms (100%), Space: 60.1 MB (59.08%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -3,20 +3,22 @@ public:
     int longestSubarray(vector<int>& nums) {
         int l = 0;
         int r = 0;
-        int n = nums.size();
+
+        int z = 0;
         int maxi = 0;
-        vector<int>v(2,0);
-        while(r < n) {
-            v[nums[r]]++;
-            if(nums[r] == 0) {
-                while(v[nums[r]] > 1) {
-                    v[nums[l]]--;
-                    l++;
-                }
+        while(r < nums.size()) {
+            if(nums[r] == 0) z++;
+
+            while(z > 1 && l < nums.size()) {
+                if(nums[l] == 0) z--;
+                l++;
             }
+
             maxi = max(maxi,r-l+1);
             r++;
         }
+
         return maxi-1;
+
     }
 };
