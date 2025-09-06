318bb5b467a7c7cf85daec1c2989191626a35036
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2025-09-06 21:11:50 +0530

    Time: 8 ms (26.42%), Space: 23.9 MB (57.16%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,28 +4,32 @@ public:
 
         sort(intervals.begin(),intervals.end());
 
+        // [1,3] [2,6] start = 2 is present in [1,3] end becomes 6 and continues 
+
         vector<vector<int>>ans;
 
+        
         int start = intervals[0][0];
         int end = intervals[0][1];
 
-        for(int i = 0 ; i < intervals.size() ; i++) {
+        for(int i = 1 ; i < intervals.size() ; i++) {
 
-            if(intervals[i][0] > end) {
+            int s = intervals[i][0];
+            int e = intervals[i][1];
 
-                ans.push_back({start,end});
-
-                start = intervals[i][0];
-                end = intervals[i][1];
+            if(s <= end && e >= end) {
+                end = e;
             }
+            else if(s <= end && e <= end) continue;
             else {
-                end = max(end,intervals[i][1]);
+                ans.push_back({start,end});
+                start = s;
+                end = e;
             }
+
+
         }
-
         ans.push_back({start,end});
-        
         return ans;
-
     }
 };
