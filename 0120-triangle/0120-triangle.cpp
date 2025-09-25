0cd6e08cc9db2c5969107a49e79233f1e3fa63ff
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2025-09-25 14:49:49 +0530

    Time: 3 ms (13.39%), Space: 13.3 MB (55.55%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,24 +1,49 @@
 class Solution {
 public:
-int fun(vector<vector<int>>&mat,int i,int j,vector<vector<int>>&dp) {
-    int m = mat.size();
-    int n = mat[i].size();
 
-    if(i == m-1 && j <= n-1) return mat[i][j];
+    // int fun(vector<vector<int>>&triangle,int i,int j,int rows,vector<vector<int>>&dp) {
 
-    if(i >= m || j >= n) return 1e9;
+    //     if(i == rows-1) return triangle[i][j];
 
-    if(dp[i][j] != -1) return dp[i][j];
+    //     if(dp[i][j] != -1) return dp[i][j];
 
-    int step1 = mat[i][j] + fun(mat,i+1,j,dp);
-    int step2 = mat[i][j] + fun(mat,i+1,j+1,dp);
+    //     int sum1 = triangle[i][j] + fun(triangle,i+1,j,rows,dp);
+    //     int sum2 = triangle[i][j] + fun(triangle,i+1,j+1,rows,dp);
 
-    return dp[i][j] = min(step1,step2);
-}
+    //     return dp[i][j] = min(sum1,sum2);
+
+
+    // }
     int minimumTotal(vector<vector<int>>& triangle) {
-        int m = triangle.size();
-        int n = triangle[m-1].size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return fun(triangle,0,0,dp);
+
+        int rows = triangle.size();
+
+        vector<vector<int>>dp(rows,vector<int>(rows,INT_MAX));
+
+        dp[0][0] = triangle[0][0];
+
+        for(int i = 1 ; i < triangle.size() ; i++) {
+
+            for(int j = 0 ; j < triangle[i].size() ; j++) {
+
+                int sum1 = INT_MAX,sum2 = INT_MAX;
+
+                if(j == 0) {
+                    sum1 = dp[i-1][j] + triangle[i][j];
+                }
+                else if(i == j) {
+                    sum2 = dp[i-1][j-1] + triangle[i][j];
+                }
+                else {
+                    sum1 = dp[i-1][j] + triangle[i][j];
+                    sum2 = dp[i-1][j-1] + triangle[i][j];
+                }
+
+                dp[i][j] = min(sum1,sum2);
+            }
+        }
+
+        return *min_element(dp[rows-1].begin(),dp[rows-1].end());
+
     }
 };
