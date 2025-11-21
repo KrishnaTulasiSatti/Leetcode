2073a680eb3cf264a23191fc80e1a5bbc94a5f2c
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2025-11-21 20:03:37 +0530

    Time: 3 ms (7.47%), Space: 10.9 MB (19.48%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,46 +1,32 @@
 class Solution {
 public:
+   
+    int fun(vector<int>&nums,int i,int n,vector<int>&dp) {
+        if(i == n-1) return nums[i];
 
-int fun(vector<int>&nums,int ind,int n,vector<int>&dp) {
+        if(i >= n) return 0;
 
-    if(ind >= n) return 0;
+        if(dp[i] != -1) return dp[i];
+
+        int pick = nums[i] + fun(nums,i+2,n,dp);
+        int not_pick = fun(nums,i+1,n,dp);
+
+        return dp[i] = max(pick,not_pick);
+    }
     
-    if(dp[ind] != -1) return dp[ind];
-    // Pick the element and move to the next next index
-
-    int pick = nums[ind]+fun(nums,ind+2,n,dp);
-
-    // Not Pick the element
-
-    int non_pick = fun(nums,ind+1,n,dp);
-
-    return dp[ind] = max(pick,non_pick);
-}
     int rob(vector<int>& nums) {
-           
-   if(nums.size() == 1) return nums[0];
+        int n = nums.size();
 
-    int n = nums.size();
-    
-    vector<int>temp1;
-    for(int i = 0 ; i < n-1 ; i++) {
-        temp1.push_back(nums[i]);
-    }
+        if(n == 1) return nums[0];
 
-    vector<int>temp2;
-    for(int i = 1 ; i < n ; i++) {
-        temp2.push_back(nums[i]);
-    }
-    
-    vector<int> dp1(temp1.size(), -1);
-    vector<int> dp2(temp2.size(), -1);
+        vector<int>dp1(n,-1);
 
-    int a = fun(temp1,0,temp1.size(),dp1);
+        int first = fun(nums,0,n-1,dp1);
 
-    int b = fun(temp2,0,temp2.size(),dp2);
+        vector<int>dp2(n,-1);
 
-    return max(a,b);
+        int last = fun(nums,1,n,dp2);
 
-     
+        return max(first,last);
     }
 };
