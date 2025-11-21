b6a48229f0c16bc0b54676ac14a473eb0408ec29
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2025-11-21 20:27:09 +0530

    Time: 0 ms (100%), Space: 9.3 MB (46.28%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
 
-int fun(int i,int j,int m,int n,vector<vector<int>>&dp) {
-    if(i == m-1 && j == n-1) return 1;
+    vector<vector<int>>dp;
 
-    if(i >= m || j >= n) return 0;
+    int fun(int i,int j,int m,int n) {
 
-    if(dp[i][j] != -1) return dp[i][j];
+        if(i == m-1 && j == n-1) return 1;
 
-    int right = fun(i,j+1,m,n,dp);
-    int bottom = fun(i+1,j,m,n,dp);
+        if(i < 0 || j < 0 || j >= n || i >= m) return 0;
 
-    return dp[i][j] = right+bottom;
-}
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int right = fun(i,j+1,m,n);
+        int bottom = fun(i+1,j,m,n);
+
+        return dp[i][j] = right + bottom;
+
+    }
+
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return fun(0,0,m,n,dp);
+
+        dp.resize(m,vector<int>(n,-1));
+        int ans = fun(0,0,m,n);
+        return ans;
     }
 };
