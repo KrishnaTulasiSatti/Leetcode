c77497e95a798d689a3218f7b4d05b020d5ebf8e
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2025-12-19 15:08:05 +0530

    Time: 0 ms (100%), Space: 14.5 MB (44.84%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,70 +1,51 @@
 class Solution {
 public:
-bool isValid(int i,int j,int m,int n) {
-    if(i < 0 || j < 0 || i >= m || j >= n) return false;
-    return true;
-}
-void bfs(int i,int j,vector<vector<int>>&state,vector<vector<char>>&mat) {
-    queue<pair<int,int>>q;
-    q.push({i,j});
-    int n = mat.size();
-    int m = mat[0].size();
-    while(!q.empty()) {
-        auto top = q.front();
-        int r = top.first;
-        int c = top.second;
-        q.pop();
-        if(isValid(r,c-1,n,m)) {
-            if(mat[r][c-1] == 'O' && state[r][c-1] == 0) {
-                state[r][c-1] = 1;
-                q.push({r,c-1});
-            }
-        }
-        if(isValid(r,c+1,n,m)) {
-            if(mat[r][c+1] == 'O'  && state[r][c+1] == 0) {
-                state[r][c+1] = 1;
-                q.push({r,c+1});
-            }
-        }
-        if(isValid(r-1,c,n,m)) {
-            if(mat[r-1][c] == 'O' && state[r-1][c] == 0) {
-                state[r-1][c] = 1;
-                q.push({r-1,c});
-            }
-        }
-        if(isValid(r+1,c,n,m)) {
-            if(mat[r+1][c] == 'O' && state[r+1][c] == 0) {
-                state[r+1][c] = 1;
-                q.push({r+1,c});
-            }
-        }
-    }
-}
-    void solve(vector<vector<char>>& mat) {
-        int m = mat.size();
-        int n = mat[0].size();
-        vector<vector<int>>state(m,vector<int>(n,0));
+    void solve(vector<vector<char>>& board) {
+        
+        queue<pair<int,int>>q;
+
+        int m = board.size();
+        int n = board[0].size();
+
         for(int i = 0 ; i < m ; i++) {
             for(int j = 0 ; j < n ; j++) {
-                if(i == 0 || i == m-1 || j == 0 || j == n-1) {
-                    if(mat[i][j] == 'O' && state[i][j] == 0) {
-                       state[i][j] = 1;
-                       bfs(i,j,state,mat);
-                    //    for(auto it : state) {
-                    //     for(auto i : it) {
-                    //         cout<<i<<" ";
-                    //     }
-                    //     cout<<endl;
-                    //    }
-                    //    cout<<endl;
+
+                if(i == 0 || j == 0 || i == m-1 || j == n-1) {
+                    if(board[i][j] == 'O') {
+                        q.push({i,j});
+                        board[i][j] = 'T';
                     }
                 }
             }
         }
 
-        for(int i = 1 ; i < m-1 ; i++) {
-            for(int j = 1 ; j < n-1 ; j++) {
-                if(state[i][j] == 0) mat[i][j] = 'X';
+        vector<int>row = {0,0,-1,1};
+        vector<int>col = {-1,1,0,0};
+
+        while(!q.empty()) {
+
+            auto top = q.front();
+            q.pop();
+
+            int r = top.first;
+            int c = top.second;
+
+            for(int i = 0 ; i < 4 ; i++) {
+                int nr = r + row[i];
+                int nc = c + col[i];
+
+                if(nr >= 0 && nr < m && nc >= 0 && nc < n && board[nr][nc] == 'O') {
+                    q.push({nr,nc});
+                    board[nr][nc] = 'T';
+                }
+            }
+
+        }
+
+        for(int i = 0 ; i < m ; i++) {
+            for(int j = 0 ; j < n ; j++) {
+                if(board[i][j] == 'O') board[i][j] = 'X';
+                if(board[i][j] == 'T') board[i][j] = 'O'; 
             }
         }
     }
