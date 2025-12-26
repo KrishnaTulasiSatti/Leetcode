b27b5e3836d4bfbbafd022e6af6daa4a071be6e5
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2025-12-26 21:44:04 +0530

    Time: 0 ms (100%), Space: 26 MB (78.04%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -6,22 +6,27 @@ public:
 
         int n = height.size();
 
-        vector<int>rMax(n,0);
-
-        rMax[n-1] = height[n-1];
-
-        for(int i = n-2 ; i >= 0 ; i--) {
-            rMax[i] = max(rMax[i+1],height[i]);
-        }
+        int l = 0;
+        int r = height.size()-1;
 
         int lMax = height[0];
+        int rMax = height[r];
 
-        for(int i = 1 ; i < n-1 ; i++) {
-          //  cout << lMax << " " << rMax[i+1] << endl;
-            total += max(0,min(lMax,rMax[i+1])-height[i]);
-            lMax = max(lMax,height[i]);
+        while(l <= r) {
+
+            if(rMax < lMax) { 
+                total += max(0,rMax-height[r]);
+                r--;
+                rMax = max(rMax,height[r]);
+            }
+            else {
+                total += max(0,lMax-height[l]);
+                l++;
+                if(l < n) lMax = max(lMax,height[l]);
+            }
         }
 
         return total;
+        
     }
 };
