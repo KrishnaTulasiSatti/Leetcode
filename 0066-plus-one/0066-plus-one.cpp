99f72d52909f07ab41f15c4348cc246aa44a736f
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2026-01-01 10:44:30 +0530

    Time: 0 ms (100%), Space: 11.1 MB (98.18%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        reverse(digits.begin(),digits.end());
+        
         int carry = 1;
-        for(int i = 0 ; i < digits.size() ; i++) {
-            int x = digits[i]+carry;
-            if(x < 10) {
-                carry = 0;
-                digits[i] = x;
-            }
-            else{
-                int a = x % 10;
-                int b = x/10;
-                digits[i] = a;
-                carry = b;
-            }
+
+        int i = digits.size()-1;
+
+        vector<int>res;
+
+        while(i >= 0 || carry) {
+
+            int sum = carry;
+
+            if(i >= 0) sum += digits[i--];
+
+            res.push_back(sum % 10);
+            carry = sum/10;
         }
-        if(carry) digits.push_back(carry);
-        reverse(digits.begin(),digits.end());
-        return digits;
+
+        reverse(res.begin(),res.end());
+
+        return res;
     }
 };
