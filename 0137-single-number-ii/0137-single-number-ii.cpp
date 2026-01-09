353acd2ef3abb805b7da3001f88db8e8a92179ed
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2026-01-09 21:15:32 +0530

    Time: 0 ms (100%), Space: 13.4 MB (64.39%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -2,19 +2,25 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         
-        int ones = 0 , twos = 0;
+        long long num = 0;
 
-        for(int i = 0 ; i < nums.size() ; i++) {
+        for(int i = 0 ; i < 32 ; i++) {
 
-            // If the number is not in twos then add it ones bucket
-            // If the number is in ones bucket then delete it from ones and add it twos
-            // If the number is not in ones bucket and is in twos bucket then delete it 
-            // from twos bucket -- So that all 3 occurrences will be cancelled
+            int cnt = 0;
 
-            ones = (ones ^ nums[i]) & (~twos);
-            twos = (twos ^ nums[i]) & (~ones);
+            for(int j = 0 ; j < nums.size() ; j++) {
+                if(nums[j] & 1) cnt++;
+                nums[j] = nums[j] >> 1;
+            }
+
+            if(cnt % 3) {
+                num += pow(2,i);
+            }
         }
 
-        return ones;
+        return num;
+
+
+
     }
 };
