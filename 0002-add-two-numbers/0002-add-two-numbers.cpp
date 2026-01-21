57576999d5a07b086fa1d22f61e30c559341df8f
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2026-01-21 23:17:41 +0530

    Time: 28 ms (7.62%), Space: 77.5 MB (12.67%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,36 +10,76 @@
  */
 class Solution {
 public:
+void fun(ListNode* temp1,int carry,ListNode* temp) {
+           int f = 0;
+           while(temp1 != NULL) {
+            int x = temp1->val+carry;
+            if(x < 10) {
+              ListNode* t = new ListNode(x);
+              temp->next = t;
+              temp = temp->next;
+              carry = 0;
+            }
+            else{
+                int a = x % 10;
+                int b = x / 10;
+                carry = b;
+                ListNode* t = new ListNode(a);
+                temp->next = t;
+                temp = temp->next;
+            }
+            temp1 = temp1->next;
+            f = 1;
+         }
+         if(f && temp1 == NULL && carry > 0) {
+            temp->next = new ListNode(carry);
+         }  
+}
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* res = new ListNode();
-
-        ListNode* temp = res;
-
         ListNode* temp1 = l1;
         ListNode* temp2 = l2;
-        
+        ListNode* res = NULL;
+        ListNode* temp = NULL;
         int carry = 0;
-       
-
-        while(temp1 != NULL || temp2 != NULL || carry) {
-
-            int sum = carry;
-
-            if(temp1) sum+=temp1->val;
-            if(temp2) sum+=temp2->val;
-
-            
-            ListNode* newnode = new ListNode(sum%10);
-            carry = sum/10;
-
-            temp->next = newnode;
-            temp = temp->next;
-
-            if(temp1) temp1 = temp1->next;
-            if(temp2) temp2 = temp2->next;
-            
-
+        while(temp1 != NULL && temp2 != NULL) {
+             int x = temp1->val+carry;
+             int y = temp2->val;
+             int sum = x+y;
+             if(sum < 10) {
+                ListNode* t = new ListNode(sum);
+                if(res == NULL) {
+                    res = t;
+                    temp = t;
+                }
+                else{
+                   temp->next = t;
+                   temp = temp->next; 
+                }
+                carry = 0;
+             }
+             else{
+                int a = sum % 10;
+                int b = sum/10;
+                carry = b;
+                ListNode* t = new ListNode(a);
+                if(res == NULL) {
+                    res = t;
+                    temp = t;
+                }
+                else{
+                   temp->next = t;
+                   temp = temp->next; 
+                }
+             }
+             temp1 = temp1->next;
+             temp2 = temp2->next;
         }
-        return res->next;
+        if(temp1 == NULL && temp2 == NULL && carry > 0) {
+            temp->next = new ListNode(carry);
+        }
+        fun(temp1,carry,temp);
+        fun(temp2,carry,temp);
+
+        return res;
     }
 };
