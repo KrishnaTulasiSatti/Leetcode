80f32b2795332bd121d60eb17a04ccb6f3131853
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2026-03-08 11:09:43 +0530

    Time: 0 ms (100%), Space: 23.3 MB (97.57%) - LeetHub

---
+++ 3861-minimum-capacity-box/3861-minimum-capacity-box.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumIndex(vector<int>& capacity, int itemSize) {
+
+        int mini = INT_MAX;
+
+        int ans  = -1;
+
+        for(int i = 0 ; i < capacity.size() ; i++) {
+            if(capacity[i] >= itemSize && capacity[i] < mini) {
+                mini = capacity[i];
+                ans = i;
+            }
+        }
+
+        return ans;
+    }
+};
