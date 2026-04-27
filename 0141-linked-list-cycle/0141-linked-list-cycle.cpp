282c7bdb50e423e84c5f65dff8e04a02311cbecf
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2026-04-27 21:46:45 +0530

    Time: 6 ms (92.34%), Space: 11.8 MB (52.08%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,16 +9,17 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        // Brute Force : Using map<ListNode*,int> if m[temp] == 1 return true;
         
         ListNode* slow = head;
         ListNode* fast = head;
 
-        while(fast != NULL && fast->next != NULL) {
+        while(slow && fast && fast->next) {
             slow = slow->next;
             fast = fast->next->next;
+
             if(slow == fast) return true;
         }
+
         return false;
     }
 };
