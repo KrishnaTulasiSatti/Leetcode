fb73353a2e3364a76d5900b47026727fb6b78da6
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2026-05-04 17:36:55 +0530

    Time: 0 ms (100%), Space: 17.6 MB (18.3%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,84 +1,40 @@
 class Solution {
 public:
+    vector<int> searchRange(vector<int>& arr, int target) {
+        
+        int n = arr.size();
 
-    int first_pos(vector<int>&nums,int target) {
-        int low = 0 , high = nums.size()-1;
-        
-        int pos = -1;
-        
+        int fs = -1;
+        int ls = -1;
+
+        int low = 0;
+        int high = n-1;
+
         while(low <= high) {
-            int mid = (low+high)/2;
+            int mid = (low + high)/2;
             
-            // Neeku mid daggara target dorikesindhi anko ans lo store
-            // chesukuntav...kaani adhe first ani cheppalem kadha so left side check 
-            // chestav ok
-            
-            if(nums[mid] == target) {
-                pos = mid;
-                high = mid-1 ; // left side ki vastundhi
+            if(arr[mid] == target) {
+                fs = mid;
+                high = mid-1;
             }
-            else {
-                // ala kadhu mid daggara target ledhu anko 
-                // binary search conditions
-                
-                if(nums[mid] < target) {
-                    low = mid+1;
-                }
-                else high = mid-1;
-            }
-            
+            else if(arr[mid] > target) high = mid-1;
+            else low = mid+1;
         }
-        return pos;
-    }
-    
-    
-     int last_pos(vector<int>&nums,int target) {
-        int low = 0 , high = nums.size()-1;
-        
-        int pos = -1;
-        
+
+        low = 0;
+        high = n-1;
+
         while(low <= high) {
-            int mid = (low+high)/2;
-           
-            if(nums[mid] == target) {
-                pos = mid;
-                low = mid+1 ; // right side ki vastundhi
+            int mid = (low + high)/2;
+
+            if(arr[mid] == target) {
+                ls = mid;
+                low = mid+1;
             }
-            else {
-                
-                if(nums[mid] < target) {
-                    low = mid+1;
-                }
-                else high = mid-1;
-            }
-            
+            else if(arr[mid] < target) low = mid+1;
+            else high = mid-1;
         }
-        return pos;
+
+        return {fs,ls};
     }
-    vector<int> searchRange(vector<int>& nums, int target) {
-      
-      
-      // Sum ardham ayyindha ha ok
-      
-      // First....first pos ki radham ok rayiii
-      
-      int first=first_pos(nums,target);
-      
-      int last = last_pos(nums,target);
-      
-      
-      
-      // checking -- first pos saripoyindhi
-      
-      // same logic last pos ki kuda kaakapothe last position neeku right side vuntundhi kabatti right side ki veltav anthe yeah ok ardamu ayindi
-      
-      
-      
-      
-      return {first,last};
-      
-      
-      
-     
-    }
-}; 
+};
