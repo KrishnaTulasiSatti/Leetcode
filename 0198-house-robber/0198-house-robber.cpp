d9163288c7991570ee62b398357d7e94f870d7f5
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2026-05-07 10:10:47 +0530

    Time: 0 ms (100%), Space: 10.5 MB (59.27%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -9,20 +9,19 @@ public:
 
         if(dp[i] != -1) return dp[i];
 
-        int pick = nums[i]+fun(nums,i+2,n);
+        //pick
+        int pick = nums[i] + fun(nums,i+2,n);
         int not_pick = fun(nums,i+1,n);
 
         return dp[i] = max(pick,not_pick);
-
-
     }
 
     int rob(vector<int>& nums) {
         int n = nums.size();
 
         dp.resize(n,-1);
-
         int ans = fun(nums,0,n);
+
         return ans;
     }
 };
