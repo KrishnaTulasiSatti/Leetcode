54e017c85f5606189d48135af21f2acf33cd9778
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2026-05-19 18:28:00 +0530

    Time: 0 ms (100%), Space: 9.3 MB (58.34%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
-vector<string>res;
 
-void fun(vector<string>&ans,string& digits,int i,string str) {
-    if(i == digits.size()-1) {
-        res.push_back(str);
-        return;
-    }
-    
-    int ind = digits[i+1]-'0';
-    for(int j = 0 ; j < ans[ind-2].size() ; j++) {
-        fun(ans,digits,i+1,str+ans[ind-2][j]);
-    }
-}
-    vector<string> letterCombinations(string digits) {
+    vector<string>ans;
 
-        vector<string>ans = {"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        if(digits.size() == 0) return {};
-        int ind = digits[0]-'0';
-        string str = "";
-        for(int i = 0 ; i < ans[ind-2].size() ; i++) {
-            fun(ans,digits,0,str+ans[ind-2][i]);
+    int n;
+
+    void fun(string& digits,int i,string& temp,vector<string>& v) {
+
+        if(i == n) {
+            ans.push_back(temp);
+            return;
         }
 
-        return res;
+        for(auto& it : v[digits[i]-'0']) {
+            temp += it;
+            fun(digits,i+1,temp,v);
+            temp.pop_back();
+        }
+
+
+    }
+
+    vector<string> letterCombinations(string digits) {
+
+        vector<string>v = {"1","1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        n = digits.size();
+
+        string temp = "";
+        fun(digits,0,temp,v);
+
+        return ans;
+
+
     }
 };
