24b134ba189150f3245192e433e26bd3dfab6457
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2026-05-19 07:19:25 +0530

    Time: 7 ms (7.62%), Space: 14.4 MB (57.46%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -2,28 +2,26 @@ class Solution {
 public:
     void solve(vector<vector<char>>& board) {
         
-        queue<pair<int,int>>q;
-
         int m = board.size();
         int n = board[0].size();
 
+        queue<pair<int,int>>q;
+
         for(int i = 0 ; i < m ; i++) {
             for(int j = 0 ; j < n ; j++) {
-
-                if(i == 0 || j == 0 || i == m-1 || j == n-1) {
+                if(i == 0 || i == m-1 || j == 0 || j == n-1) {
                     if(board[i][j] == 'O') {
                         q.push({i,j});
-                        board[i][j] = 'T';
+                        board[i][j] = 'N';
                     }
                 }
             }
         }
 
-        vector<int>row = {0,0,-1,1};
-        vector<int>col = {-1,1,0,0};
+        vector<int>dr = {-1,1,0,0};
+        vector<int>dc = {0,0,-1,1};
 
         while(!q.empty()) {
-
             auto top = q.front();
             q.pop();
 
@@ -31,22 +29,31 @@ public:
             int c = top.second;
 
             for(int i = 0 ; i < 4 ; i++) {
-                int nr = r + row[i];
-                int nc = c + col[i];
+                int nr = r + dr[i];
+                int nc = c + dc[i];
 
-                if(nr >= 0 && nr < m && nc >= 0 && nc < n && board[nr][nc] == 'O') {
-                    q.push({nr,nc});
-                    board[nr][nc] = 'T';
+                if(nr >= 0 && nr < m && nc >= 0 && nc < n) {
+                    if(board[nr][nc] == 'O') {
+                        q.push({nr,nc});
+                        board[nr][nc] = 'N';
+                    }
                 }
             }
+        }
 
+        
+        for(int i = 0 ; i < m ; i++) {
+            for(int j = 0 ; j < n ; j++) {
+                if(board[i][j] == 'O') board[i][j] = 'X';
+            }
         }
 
         for(int i = 0 ; i < m ; i++) {
             for(int j = 0 ; j < n ; j++) {
-                if(board[i][j] == 'O') board[i][j] = 'X';
-                if(board[i][j] == 'T') board[i][j] = 'O'; 
+                if(board[i][j] == 'N') board[i][j] = 'O';
             }
         }
+
+
     }
 };
