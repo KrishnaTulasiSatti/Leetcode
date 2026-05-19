d6c20b9eb3777f1770c77f8681beff0fb1bd79de
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2026-05-19 15:01:24 +0530

    Time: 2 ms (36.94%), Space: 9.9 MB (80.47%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -3,22 +3,27 @@ public:
 
     vector<vector<int>>ans;
 
-    void fun(vector<int>&nums,int i,int n,vector<int>&temp) {
+    void fun(int i,int n,vector<int>&temp,vector<int>&nums) {
+        
         if(i == n) {
             ans.push_back(temp);
             return;
         }
 
         temp.push_back(nums[i]);
-        fun(nums,i+1,n,temp);
+        fun(i+1,n,temp,nums);
         temp.pop_back();
-        fun(nums,i+1,n,temp);
 
+        fun(i+1,n,temp,nums);
     }
+
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n = nums.size();
+        
         vector<int>temp;
-        fun(nums,0,n,temp);
+        int n = nums.size();
+
+        fun(0,n,temp,nums);
         return ans;
+
     }
 };
