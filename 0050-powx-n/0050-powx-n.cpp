620161e2f877f90744f45c3c1c64fa7f312361e9
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2026-05-19 12:37:13 +0530

    Time: 0 ms (100%), Space: 8.5 MB (92.43%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
+
     double fun(double x,long long n) {
         if(n == 0) return 1.0;
-         
         if(n == 1) return x*1.0;
 
         if(n % 2) return x * fun(x,n-1);
-
         return fun(x*x,n/2);
     }
+    
     double myPow(double x, int n) {
-        int flag = false;
+        
+        bool flag = false;
 
         if(n < 0) flag = true;
 
         long long num = n;
-        
+
         if(flag) num = -1*num;
 
         double ans = fun(x,num);
 
         if(flag) return 1.0/ans;
-
+        
         return ans;
     }
 };
