d7f4dfe601fdf6f37d62425f7dff56dfa4236c87
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2026-05-19 18:14:36 +0530

    Time: 0 ms (100%), Space: 8.9 MB (38.17%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
-    vector<vector<int>>res;
 
-    void fun(int i,int n,int k,vector<int>&ans) {
-        if(i == 10) {
-            int sum = accumulate(ans.begin(),ans.end(),0);
-            if(sum == n && ans.size() == k) res.push_back(ans);
+    vector<vector<int>>ans;
+
+    void fun(vector<int>& nums,int i,vector<int>& temp,int n,int k) {
+
+        if(i == 9) {
+            if(temp.size() == k) {
+                if(n == 0) ans.push_back(temp);
+            }
             return;
         }
 
-        // pick
-        ans.push_back(i);
-        fun(i+1,n,k,ans);
+        if(n < 0 || temp.size() > k) return;
 
-        // not pick
-        ans.pop_back();
-        fun(i+1,n,k,ans);
+        temp.push_back(nums[i]);
+        fun(nums,i+1,temp,n-nums[i],k);
+        temp.pop_back();
+
+        fun(nums,i+1,temp,n,k);
+
     }
+
     vector<vector<int>> combinationSum3(int k, int n) {
         
-        vector<int>ans;
-        fun(1,n,k,ans);
-        return res;
+        vector<int>nums = {1,2,3,4,5,6,7,8,9};
+
+        vector<int>temp;
+
+        fun(nums,0,temp,n,k);
+
+        return ans;
+
+
     }
 };
