969358fb450725489b2dbcb7922ac35a03824374
Satti Krishna Tulasi <147472962+KrishnaTulasiSatti@users.noreply.github.com>
2026-05-20 17:33:06 +0530

    Time: 382 ms (31.16%), Space: 8.9 MB (37.08%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+    bool valid(vector<vector<char>>& board,int r,int c,char ch) {
+
+        for(int i = 0 ; i < 9 ; i++) {
+
+            if(board[r][i] == ch) return false;
+
+            if(board[i][c] == ch) return false;
+
+            if(board[3*(r/3)+(i/3)][3*(c/3)+(i%3)] == ch) return false;
+        }
+
+        return true;
+    }
+    
+    bool fun(vector<vector<char>>& board) {
+
+        for(int i = 0 ; i < 9 ; i++) {
+            for(int j = 0 ; j < 9 ; j++) {
+
+                if(board[i][j] == '.') {
+                    
+                    for(int ch = 1 ; ch <= 9 ; ch++) {
+                        if(valid(board,i,j,ch+'0')) {
+                            board[i][j] = ch+'0';
+                            if(fun(board) == true) return true;
+                            else board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        fun(board);
+    }
+};
